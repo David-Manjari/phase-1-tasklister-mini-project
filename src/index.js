9344bcde37474ccf0e868673edc4214591fb7e8c
Manjari <manjari@Manjari.localdomain>
2026-04-27 21:13:07 +0300

    Created a dom to manipulate a to do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form")
+  const newTask = document.getElementById("new-task-description")
+  const taskToComplete = document.getElementById("tasks")
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const listTask = document.createElement('li')
+    listTask.textContent = newTask.value;
+
+    taskToComplete.appendChild(listTask);
+    listTask.style.listStyle = "numbers"
+
+    newTask.value = "";
+
+
+  })
+
 });
